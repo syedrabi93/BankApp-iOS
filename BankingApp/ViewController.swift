3546b45cae23d3e2346a620746a13a73faa8910e
Syed Rabiyama S <syedrabi@amazon.com>
2020-11-26 09:58:59 +0530

    Initial Commit

---
+++ BankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by S, Syed Rabiyama on 26/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
