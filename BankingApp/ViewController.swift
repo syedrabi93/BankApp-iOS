bbfd2d87210d24896f30dcfe1a3a0fffd235aa4e
Syed Rabiyama S <syedrabi@amazon.com>
2020-11-27 18:31:43 +0530

    first commit

--- BankingApp/ViewController.swift
+++ BankingApp/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Uname: UITextField!
+    @IBOutlet weak var Pword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
